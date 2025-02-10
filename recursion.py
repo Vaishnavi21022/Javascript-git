60b1568292e8976f40141f9f0523d843a7bf6572
Vaishnavi-Jani <vaishujan.02@gmail.com>
2025-02-10 23:53:39 +0530

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,15 @@
+def fn():
+    print("Helloworld")
+    fn1()
+def fn1():
+    print("Helloworld")
+    fn2()
+def fn2():
+    print("Helloworld")
+    fn3()
+def fn3():
+    print("Helloworld")
+    fn4()
+def fn4():
+    print("Helloworld")
+fn()
